8b85e994c6afa0e5033ec57b04b8860679790dd9
Елена Орляхина <sconoscuita@gmail.com>
2026-02-03 18:33:38 +0400

    my_test3

--- cypress/e2e/01.login/login.cy.js
+++ cypress/e2e/01.login/login.cy.js
@@ -2,7 +2,7 @@
 describe('Вход в систему', () => {
     let testData;
     before(() => {
-        cy.fixtures('test_user').then((data) => {
+        cy.fixture('test_user').then((data) => {
             testData = data;
         });
     });
@@ -13,10 +13,18 @@ describe('Вход в систему', () => {
 
     //тест успешного входа
     it('Успешная авторизация', () => {
-        cy.get('input[name="username"]').type(testData.validUser.username);
+        cy.get('input[name="username"]').type(testData.validUser.login);
         cy.get('input[name="password"]').type(testData.validUser.password);
         cy.get('button[type="submit"]').click();
         cy.url().should('include', '/dashboard');
         cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'Dashboard');
+    });
+
+    //тест неуспешного входа
+    it('Неуспешная авторизация', () => {
+        cy.get('input[name="username"]').type(testData.invalidUser.login);
+        cy.get('input[name="password"]').type(testData.invalidUser.password);
+        cy.get('button[type="submit"]').click();
+        cy.get('div[role="alert"]').should('contain', 'Invalid credentials');
     })
 });
